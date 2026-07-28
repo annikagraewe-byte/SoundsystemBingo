faa41a5042f1927025d2a777f65c68b4229b7701
annikagraewe-byte <annikagraewe@googlemail.com>
2026-07-28 22:12:04 +0200

    Update Data

--- data.js
+++ data.js
@@ -14,7 +14,16 @@ const TRACKS = [
   { position: 3,  title: "Sounds Like a Melody",       artist: "Alpahville",         guest: "Julia" },
   { position: 4,  title: "ミラクルショッピング〜恋に落ちて〜",       artist: "RSP",         guest: "Mila & Julian" },
   { position: 5,  title: "Where is My Mind",       artist: "Maxence Cyrin",         guest: "Annika" },
-  { position: 6,  title: "Sozusagen grundlos vergnügt",       artist: "Dota Kehr",         guest: "Anabelle" }
+  { position: 6,  title: "We didn't Start the Fire",       artist: "Hildegard von Blingin'",         guest: "Jan" },
+  { position: 7,  title: "Part of the Machine",       artist: "Die Krupps",         guest: "Alex & Maw" },
+  { position: 8,  title: "Blank Space",       artist: "Taylor Swift",         guest: "Geno" },
+  { position: 9,  title: "Experience",       artist: "Ludovico Einaudi",         guest: "LJ" },
+  { position: 10,  title: "You're Welcome",       artist: "Dwayne Johnson",         guest: "Sabrina & Marvin" },
+  { position: 11,  title: "Nabucco - Va, pensiero",       artist: "Guiseppe Verdi",         guest: "Simona" },
+  { position: 12,  title: "Pump It",       artist: "Electric Callboy",         guest: "Erika" },
+  { position: 13,  title: "Ich war noch niemals in New York",       artist: "Udo Jürgens",         guest: "Manuela" },
+  { position: 14,  title: "Wat mutt, dat mutt",       artist: "Godewind",         guest: "Manfred" },
+  { position: 15,  title: "Sozusagen grundlos vergnügt",       artist: "Dota Kehr",         guest: "Anabelle" }
 ];
 
 /* Anzahl der Bingokarten, die für den Druck erzeugt werden */
